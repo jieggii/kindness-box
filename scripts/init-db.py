fd6e969a9d894b0e9d74a75a3471545eb0817083
jieggii <jieggii@pm.me>
2022-11-16 14:55:03 +0200

    Some fixes, format code

--- scripts/init-db.py
+++ scripts/init-db.py
@@ -19,8 +19,12 @@ def parse_args():
         prog="init-db.py",
         description="Use this script to initialize kindness-box database",
     )
-    parser.add_argument("-x", "--exec", type=argparse.FileType(), default=None, help="SQL script to run at first")
-    parser.add_argument("-e", "--env", type=argparse.FileType(), default=None, help=".env file to use")
+    parser.add_argument(
+        "-x", "--exec", type=argparse.FileType(), default=None, help="SQL script to run at first"
+    )
+    parser.add_argument(
+        "-e", "--env", type=argparse.FileType(), default=None, help=".env file to use"
+    )
     parser.add_argument("file", type=argparse.FileType("rb"), help="XLSX file in special format")
     # Excel table format:
     # id | Name and lastname | Age | Locality | Gift description |
@@ -32,6 +36,7 @@ def main():
     args = parse_args()
     if args.env:
         import dotenv
+
         dotenv.load_dotenv(stream=args.env)
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
@@ -44,7 +49,9 @@ def main():
     from app import db
     from app.config import config
 
-    print(f"[*] Connecting to {config.PG.USER}@{config.PG.HOST:{config.PG.PORT}}/{config.PG.DATABASE}")
+    print(
+        f"[*] Connecting to postgresql at {config.PG.USER}@{config.PG.HOST}:{config.PG.PORT}/{config.PG.DATABASE}"
+    )
     loop.run_until_complete(db.init())
 
     print("[*] Adding points to the database")
@@ -52,7 +59,23 @@ def main():
     for i, locality_enum in enumerate(db.models.PointLocality):
         point_id = i + 1
         point_locality = locality_enum.value
-        point = db.models.Point(point_id=point_id, locality=point_locality, address=f"...")  # todo: do anything with it?
+        # some hardcoded shit (todo)
+        if locality_enum == db.models.PointLocality.PETROZAVODSK:
+            address = "с понедельника по пятницу в региональный офис Общероссийского Народного Фронта (ул. Дзержинского, д. 3, каб. 4) с 9 до 17, перерыв с 13 до 14"
+        elif locality_enum == db.models.PointLocality.KOSTOMUKSHA:
+            address = "с понедельника по пятницу в здание администрации (ул. Строителей, д. 5, каб. 111) с 9 до 17, перерыв с 12.30 до 14 или в средней школе № 2 имени А.С. Пушкина (ул. Ленина, д. 19, вахта) с 14 до 19"
+        elif locality_enum == db.models.PointLocality.MUEZERSKIY:
+            address = (
+                "с понедельника по пятницу в Дом Творчества (пер. Строителей, д. 13) с 10 до 19"
+            )
+        elif locality_enum == db.models.PointLocality.KALEVALA:
+            address = "<unknown>"  # todo
+        else:
+            raise ValueError()
+
+        point = db.models.Point(
+            point_id=point_id, locality=point_locality, address=address
+        )  # todo: do anything with it?
         loop.run_until_complete(point.save())
         points[point_locality] = point
         print(f'[+] Added new point "{point_locality}", point_id={point_id}.')
@@ -68,17 +91,25 @@ def main():
         age = row[2].value
         locality = row[3].value
         gift = row[4].value
+
         if not (number and name and age and locality and age):
-            print(f"[!] Warning: it seems that end of the list is reached. Last person number: {int(number-1)}.")
+            print(
+                f"[!] Warning: it seems that end of the list is reached. Last person number: {int(number-1)}."
+            )
             break
 
         number = int(number)
+        name = name.strip()
         age = int(age)
+        locality = locality.strip()
+        gift = gift.strip()
 
         if locality not in points.keys():
             print(f'[!] Fatal error: unknown locality "{locality}" (person number: {number}).')
             sys.exit(1)
-        person = db.models.Person(person_id=number, name=name, age=age, gift=gift, point=points[locality])
+        person = db.models.Person(
+            person_id=number, name=name, age=age, gift=gift, point=points[locality]
+        )
         loop.run_until_complete(person.save())
         print(f"[+] Added new person {name}, person_id={number}.")
 
