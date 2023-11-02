6d3941f104970d052b30fddb85bdea6266f85930
jieggii <jieggii@protonmail.com>
2023-11-02 19:12:44 +0200

    Update comments in `bot.settings`

--- bot/settings.py
+++ bot/settings.py
@@ -1,3 +1,8 @@
-DEADLINE = "16 декабря 2022 года"  # deadline (will be displayed in a message)
-MAX_ORGANIZATION_NAME_LENGTH = 500  # max length of organization name
-MAX_CHOSEN_RECIPIENTS = 5  # max amount of recipients that can be chosen for one recipient
+# deadline (will be displayed in a message):
+DEADLINE = "16 декабря 2022 года"
+
+# max length of organization name:
+MAX_ORGANIZATION_NAME_LENGTH = 500
+
+# max amount of recipients that can be chosen for one recipient:
+MAX_CHOSEN_RECIPIENTS = 5
