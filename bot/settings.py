0c7c181bfaca9b7641b8d0991dff192c8a5fc751
jieggii <jieggii@protonmail.com>
2023-11-02 19:03:14 +0200

    Add comment for `MAX_CHOSEN_RECIPIENTS` constant

--- bot/settings.py
+++ bot/settings.py
@@ -1,3 +1,3 @@
 DEADLINE = "16 декабря 2022 года"  # deadline (will be displayed in a message)
 MAX_ORGANIZATION_NAME_LENGTH = 500  # max length of organization name
-MAX_CHOSEN_RECIPIENTS = 5
+MAX_CHOSEN_RECIPIENTS = 5  # max amount of recipients that can be chosen for one recipient
